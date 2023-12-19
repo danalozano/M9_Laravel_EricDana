9dcab81f76d218936c46aec067dd1d9a083ba919
ericsaza-InsBc <eric.salado@insbaixcamp.cat>
2023-12-19 20:03:27 +0100

    Seeder destinos añadidos 3 mas

--- database/seeders/DestinoSeeder.php
+++ database/seeders/DestinoSeeder.php
@@ -15,23 +15,44 @@ class DestinoSeeder extends Seeder
     {
         Destino::create([
             'nombre' => 'Madrid',
-            'descripcion' => 'Centro turistico de España',
+            'descripcion' => 'Centro turistico de España.',
             'precio' => 1000.00,
             'pais' => 'España',
         ]);
 
         Destino::create([
             'nombre' => 'Barcelona',
-            'descripcion' => 'Capital de la provincia de Cataluña',
+            'descripcion' => 'Capital de la provincia de Cataluña.',
             'precio' => 2000.50,
             'pais' => 'España',
         ]);
 
         Destino::create([
             'nombre' => 'Paris',
-            'descripcion' => 'Capital de Francia',
+            'descripcion' => 'Capital de Francia.',
             'precio' => 0.50,
             'pais' => 'Francia',
+        ]);
+
+        Destino::create([
+            'nombre' => 'Tokio',
+            'descripcion' => 'La torre de Tokio.',
+            'precio' => 3000.00,
+            'pais' => 'Japon',
+        ]);
+
+        Destino::create([
+            'nombre' => 'Singapur',
+            'descripcion' => 'Viajes de ensueño ideado por Dana.',
+            'precio' => 5000.00,
+            'pais' => 'Singapur',
+        ]);
+
+        Destino::create([
+            'nombre' => 'Cali',
+            'descripcion' => 'Monument Cristo Rey.',
+            'precio' => 4000.00,
+            'pais' => 'Colombia',
         ]);
     }
 }
