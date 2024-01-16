9b97712964766fa54de12a983154aebf106e715e
danaguzman <danalozano0612@gmail.com>
2024-01-16 19:09:43 +0100

    añadir rutas

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/destino', [DestinoController::class, 'index']);
+Route::get('/destino/{id}', [DestinoController::class, 'show']);
+
 require __DIR__.'/auth.php';
