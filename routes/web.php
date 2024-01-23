7873c8dd1ba9a5dc977ae20f4268c7ba240e88f0
EdwinAMV <edalbert1407@gmail.com>
2024-01-23 17:12:40 +0100

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,12 @@ Route::middleware('auth')->group(function () {
 Route::get('/destino', [DestinoController::class, 'index']);
 Route::get('/destino/{id}', [DestinoController::class, 'show']);
 
+Route::get('/destinos', [DestinoController::class, 'index'])->name('destino.index');
+Route::get('/destinos/create', [DestinoController::class, 'create'])->name('destino.create');
+Route::post('/destinos', [DestinoController::class, 'store'])->name('destino.store');
+Route::get('/destinos/{destino}', [DestinoController::class, 'show'])->name('destino.show');
+Route::get('/destinos/{destino}/edit', [DestinoController::class, 'edit'])->name('destino.edit');
+Route::put('/destinos/{destino}', [DestinoController::class, 'update'])->name('destino.update');
+Route::delete('/destinos/{destino}', [DestinoController::class, 'destroy'])->name('destino.destroy');
+
 require __DIR__.'/auth.php';
